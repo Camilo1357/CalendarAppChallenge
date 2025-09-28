dcaa54193d10556e13c6ae0c0e8d7a523f796d78
camilo acosta <camiloesteban3007@outlook.com>
2025-09-27 21:33:05 -0500

    Commit of class day

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -31,14 +31,37 @@ class Event:
     def add_reminder(self, date_time: datetime, type: str):
         self.reminders.append(Reminder(date_time, type))
 
-    def delete_
+    def delete_reminder(self, reminder_index: int ):
+        if 0 <= reminder_index < len(self.reminders):
+            self.reminders.pop(reminder_index)
+        else:
+            reminder_not_found_error()
 
-
-
-
-
+    def __str__(self):
+        return f"ID: {self.id}\n Event title: {self.title}\n Description: {self.description}\n Time: {self.start_at} - {self.end_at}"
 
 # TODO: Implement Day class here
+class Day:
+    def __init__(self, date_: date):
+        self.date_ : date = date_
+        self.slots : dict[time, str | None] = {}
+        self._init_slots()
+
+    def _init_slots(self):
+        for hour in range(0, 24):
+            for minute in [0, 15, 30, 45]:
+                time_slot = time(hour, minute)
+                self.slots[time_slot] = None
+
+    def add_event(self,event_id: str, start_at: time, end_at: time):
+        self.slots.append(event_id)
+
+
+
+
+
+
+
 
 
 # TODO: Implement Calendar class here
